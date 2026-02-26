18930118e4d2b621df2792efd1788e3eb7eace6a
Taufeeq Ahmed <taufeeq6t5@gmail.com>
2026-02-26 19:16:46 +0530

    Update Step-4_ML_implementation.py

--- Step-4_ML_implementation.py
+++ Step-4_ML_implementation.py
@@ -7,7 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
-
 # ------------------------------
 # 2. Connect to MongoDB
 # ------------------------------
@@ -15,12 +14,10 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["Electric_Vehicle_project"]
 collection = db["ElectricVehicle_data"]
 
-
 # ------------------------------
 # 3. Fetch Data from MongoDB
 # ------------------------------
 data = list(collection.find())
-
 
 # ------------------------------
 # 4. Convert to DataFrame
@@ -29,7 +26,6 @@ df = pd.DataFrame(data)
 
 print("First 5 rows from MongoDB:")
 print(df.head())
-
 
 # ------------------------------
 # 5. Data Cleaning
@@ -44,7 +40,6 @@ df = df.dropna()
 # Convert categorical columns into numbers (One-Hot Encoding)
 df = pd.get_dummies(df, drop_first=True)
 
-
 # ------------------------------
 # 6. Define Features & Target
 # ------------------------------
@@ -54,7 +49,6 @@ y = df["range_km"]
 # All remaining columns are features
 X = df.drop("range_km", axis=1)
 
-
 # ------------------------------
 # 7. Split Data
 # ------------------------------
@@ -62,19 +56,16 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-
 # ------------------------------
 # 8. Train Model
 # ------------------------------
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-
 # ------------------------------
 # 9. Predict
 # ------------------------------
 y_pred = model.predict(X_test)
-
 
 # ------------------------------
 # 10. Evaluate Model
